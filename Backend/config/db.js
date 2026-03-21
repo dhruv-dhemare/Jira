e0b26600de61832207a784ba47712bbefdf28d3e
dhruv-dhemare <dhruvdhemare@gmail.com>
2026-03-21 15:18:31 +0530

    The Neon db is added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "myapp",
-  password: "D@11230526",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
