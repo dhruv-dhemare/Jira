50503261cd3577f567dbfc5095ee2229e3bd2c9e
dhruv-dhemare <dhruvdhemare@gmail.com>
2026-04-02 14:57:36 +0530

    corrected the cors

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // 🔥 CORS MIDDLEWARE (VERY IMPORTANT)
 app.use(
   cors({
-    origin: "https://sprint-hub.netlify.app/",
+    origin: "https://sprint-hub.netlify.app",
     credentials: true,
   })
 );
